8ae8e2f6acb4653c76bad3ae94ff75b2df3d07c1
Christopher Dembia <cld72@cornell.edu>
2016-06-29 18:45:40 -0700

    Try to run on travis.

--- auto_vs_finite.cpp
+++ auto_vs_finite.cpp
@@ -1,52 +1,81 @@
 #include <adolc/adolc.h>
 #include <iostream>
 #include <iterator>
+#include <random>
+#include <cassert>
 
+// TODO profile second evaluate of the jacobian.
+
+/// This is a dense function R^n -> R^m. It is templatized so that we can
+/// evaluate it on both double and adouble.
 template <typename T>
-void func(int n, int m, const T* x, T* y) {
-    y[m-1] = 0;
+void constraint_function_dense(int n, int m, const T* x, T* y) {
+    for (int j = 0; j < m; ++j) y[j] = 0; 
     for (int i = 0; i < n; ++i) {
-        y[m-1] += x[i] * x[i];
+        for (int j = 0; j < m; ++j) {
+            y[j] += log(j + 2) * cos(x[i]) * x[(n - 1) - i];
+        }
     }
 }
 
-void auto_diff(int n, int m, const double* px, double** J) {
+/// This differentiates a function R^n -> R^m using ADOL-C.
+void auto_jacobian(int n, int m, const double* px, double** J) {
+
     short int tag = 0;
+
+    // Start recording.
     trace_on(tag);
 
     std::vector<adouble> x(n);
     std::vector<adouble> y(m);
-    std::vector<double> py(m);
+    std::vector<double> py(m); // p for "passive variable;" ADOL-C terminology.
 
+    // Indicate independent variables.
     for (int i = 0; i < n; ++i) x[i] <<= px[i];
 
-    func(n, m, x.data(), y.data());
+    // Evaluate function.
+    constraint_function_dense(n, m, x.data(), y.data());
 
+    // Indicate dependent variables.
     for (int j = 0; j < m; ++j) y[j] >>= py[j];
 
+    // Stop recording.
     trace_off();
 
+    // Use the recorded tape to compute the jacobian.
     int success = jacobian(tag, m, n, px, J);
+    assert(success == 3);
 }
 
-void finite_diff(int n, int m, const double* x, double** J) {
-    std::vector<double> f(m);
-    func(n, m, x, f.data());
-    std::vector<double> f_perturb(m);
-    double h = 1e-9; // sweet spot!
+/// This differentiates a function R^n -> R^m using central finite differences.
+void finite_jacobian(int n, int m, const double* x, double** J) {
+
+    // Working memory for storing the perturbations.
+    std::vector<double> f_perturb_right(m);
+    std::vector<double> f_perturb_left(m);
+    std::vector<double> x_perturb(x, x + n); 
+    double h = 1e-9; // time step. sweet spot based on quick testing.
+    // Loop through the independent variables.
     for (int j = 0; j < n; ++j) {
-        // TODO use the memory once; just keep twiddling with the elements.
-        std::vector<double> x_perturb(x, x + n);
         x_perturb[j] += h;
-        func(n, m, x_perturb.data(), f_perturb.data());
+        constraint_function_dense(n, m, x_perturb.data(), f_perturb_right.data());
+        // 2 * h to cancel out the +h perturbation.
+        x_perturb[j] -= 2 * h;
+        constraint_function_dense(n, m, x_perturb.data(), f_perturb_left.data());
+        // Restore this element to its original value, in preparation for the
+        // next perturbation.
+        x_perturb[j] = x[j];
+
+        // Record the derivative for each row i for this column j.
         for (int i = 0; i < m; ++i) {
-            J[i][j] = (f_perturb[i] - f[i]) / h;
+            // df/dx = [f(x + h) - f(x + h)] / (2h)
+            J[i][j] = (f_perturb_right[i] - f_perturb_left[i]) / (2 * h);
         }
     }
 }
 
 std::ostream& operator<<(std::ostream& o, std::vector<double>& v) {
-    for (const auto& e : v) { o << e; }
+    for (const auto& e : v) { o << e << " "; }
     return o;
 }
 
@@ -61,24 +90,56 @@ void print_and_clear(int n, int m, double* const * J) {
     }
 }
 
-int main() {
-    // TODO random.
-    // TODO first do it on a dense matrix.
-    std::vector<double> x {1, 2, 3, 4, 5};
-    std::vector<double> y(1);
-    func(x.size(), y.size(), x.data(), y.data());
-    std::cout << "f(x): " << y << std::endl;
+template<typename Function>
+double timeit(Function f) {
+    using namespace std::chrono;
+    auto start = steady_clock::now();
+    // ------------------------------ //
+    /* calling */   f();  /* function */
+    // ------------------------------ //
+    auto end = steady_clock::now();
+    auto dur = duration_cast<milliseconds>(end - start).count();
+    std::cout << "Duration: " << dur << " milliseconds" << std::endl;
+    return dur;
+}
 
-    // TODO  double** J = new double[x.size()][y.size()];
+int main() {
+    // Benchmarking on a dense jacobian.
+    int n = 1000; int m = 1000; // i.e., 100 nodes, 10 states
+
+    // Generate a random point x.
+    // For consistent results, use same seed each time.
+    std::default_random_engine generator(0); 
+    // Uniform distribution between 0.1 and 0.9.
+    std::uniform_real_distribution<double> distribution(-0.5, 0.5);
+    std::vector<double> x(n);
+    std::generate(x.begin(), x.end(), [&]() { return distribution(generator); });
+
+    // For fun, evaluate the constraint function at x.
+    std::vector<double> y(m);
+    constraint_function_dense(x.size(), y.size(), x.data(), y.data());
+    // TODO std::cout << "f(x): " << y << std::endl;
+
+    // TODO double** J = new double[x.size()][y.size()];
     double** J = new double*[y.size()];
     for (int i = 0; i < y.size(); ++i) J[i] = new double[x.size()];
 
-    auto_diff(x.size(), y.size(), x.data(), J);
-    print_and_clear(x.size(), y.size(), J);
+    std::cout << "Computing with automatic differentiation... ";
+    auto autodur = timeit([&]() {
+            auto_jacobian(x.size(), y.size(), x.data(), J);
+            });
+    // std::cout << "J(x):" << std::endl;
+    // print_and_clear(x.size(), y.size(), J);
 
-    finite_diff(x.size(), y.size(), x.data(), J);
-    print_and_clear(x.size(), y.size(), J);
+    std::cout << "Computing with finite differences... ";
+    auto finitedur = timeit([&]() {
+            finite_jacobian(x.size(), y.size(), x.data(), J);
+            });
+    // std::cout << "J(x):" << std::endl;
+    // print_and_clear(x.size(), y.size(), J);
 
-    // TODO leak delete[][] J;
+    std::cout << "finite / auto: " << finitedur / autodur << std::endl;
+
+    // TODO fix leak delete[][] J;
     return EXIT_SUCCESS;
 }
---
+++ other/main.cpp
@@ -0,0 +1,5 @@
+static void A() {}
+
+int main() {
+    A();
+}
